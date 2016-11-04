e2bbc971b13eec4985f25efff330d8bd93893825
Hajt5 <hajtusgn@gmail.com>
2016-11-04 17:09:44 +0100

    Add files via upload

---
+++ install.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `warmod_matches` (
+  `match_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `team_a_name` varchar(128) NOT NULL,
+  `team_b_name` varchar(128) NOT NULL,
+  `CURRENT_MAP` varchar(64) NOT NULL,
+  `team_size` int(11) UNSIGNED NOT NULL,
+  `team_a_t_score` int(11) UNSIGNED NOT NULL,
+  `team_b_ct_score` int(11) UNSIGNED NOT NULL,
+  `team_a_ct_score` int(11) UNSIGNED NOT NULL,
+  `team_b_t_score` int(11) UNSIGNED NOT NULL,
+  `mix_end_time` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`match_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `warmod_stats` (
+  `stats_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `match_id` int(11) UNSIGNED NOT NULL,
+  `usgn` int(11) UNSIGNED NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `flag` varchar(64) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `team` tinyint(1) UNSIGNED NOT NULL,
+  `mix_dmg` int(11) UNSIGNED NOT NULL,
+  `total_kills` int(11) UNSIGNED NOT NULL,
+  `total_deaths` int(11) UNSIGNED NOT NULL,
+  `bomb_plants` int(11) UNSIGNED NOT NULL,
+  `bomb_defusals` int(11) UNSIGNED NOT NULL,
+  `double` int(11) UNSIGNED NOT NULL,
+  `triple` int(11) UNSIGNED NOT NULL,
+  `quadra` int(11) UNSIGNED NOT NULL,
+  `aces` int(11) UNSIGNED NOT NULL,
+  `total_mvp` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`stats_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ warmod.php
@@ -0,0 +1,131 @@
+<?php
+$dbhost = 'dbhost';
+$dbuser = 'dbuser';
+$dbpasswd = 'dbpasswd';
+$dbname = 'dbname';
+$email = 'user@example.com';
+$port = 12111;
+
+function escape_string($link, $var)
+{
+	$var = mysqli_real_escape_string($link, $var);
+	$var = utf8_encode($var);
+	$var = trim($var);
+	$var = htmlspecialchars($var);
+
+	return $var;
+}
+
+function send_results($data)
+{
+	global $dbhost;
+	global $dbuser;
+	global $dbpasswd;
+	global $dbname;
+	global $email;
+	global $id;
+
+	$length = sizeof($data);
+	printf("Data length: %d\n", $length);
+
+	$link = mysqli_connect($dbhost, $dbuser, $dbpasswd, $dbname);
+
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+
+	mysqli_set_charset($link, "utf8");
+
+	if ($length == 8) {
+		$team_a = escape_string($link, $data[0]);
+		$team_b = escape_string($link, $data[1]);
+		$map = escape_string($link, $data[2]);
+		$size = escape_string($link, $data[3]);
+		$team_a_t_score = escape_string($link, $data[4]);
+		$team_b_ct_score = escape_string($link, $data[5]);
+		$team_a_ct_score = escape_string($link, $data[6]);
+		$team_b_t_score = escape_string($link, $data[7]);
+		$end = time();
+
+		$query = "INSERT INTO warmod_matches VALUES (NULL, '$team_a', '$team_b', '$map', '$size', '$team_a_t_score', '$team_b_ct_score', '$team_a_ct_score', '$team_b_t_score', '$end')";
+
+		mysqli_query($link, $query);
+
+		$id = mysqli_insert_id($link);
+		print("New record has id: $id\n");
+	}
+
+	if ($length == 14) {
+		$usgn = escape_string($link, $data[0]);
+		$ip = escape_string($link, $data[1]);
+
+		$iphub = file_get_contents("http://legacy.iphub.info/api.php?ip=$ip&showtype=4&email=$email");
+		$json = json_decode($iphub, true);
+		if (array_key_exists("countryCode", $json)) {
+			$flag = strtolower($json["countryCode"]);
+			if ($flag == "") {
+				$flag = "unknown";
+			}
+		} else {
+			$flag = "unknown";
+		}
+
+		$name = escape_string($link, $data[2]);
+		$team = escape_string($link, $data[3]);
+		$mix_dmg = escape_string($link, $data[4]);
+		$frags = escape_string($link, $data[5]);
+		$deaths = escape_string($link, $data[6]);
+		$bomb_plants = escape_string($link, $data[7]);
+		$bomb_defusals = escape_string($link, $data[8]);
+		$double = escape_string($link, $data[9]);
+		$triple = escape_string($link, $data[10]);
+		$quadra = escape_string($link, $data[11]);
+		$aces = escape_string($link, $data[12]);
+		$total_mvp = escape_string($link, $data[13]);
+
+		$query = "INSERT INTO warmod_stats VALUES (NULL, '$id', '$usgn', '$ip', '$flag', '$name', '$team', '$mix_dmg', '$frags', '$deaths', '$bomb_plants', '$bomb_defusals', '$double', '$triple', '$quadra', '$aces', '$total_mvp')";
+
+		mysqli_query($link, $query);
+	}
+
+	mysqli_close($link);
+}
+
+if (!($socket = socket_create(AF_INET, SOCK_DGRAM, 0))) {
+	$errorcode = socket_last_error();
+	$errormsg = socket_strerror($errorcode);
+
+	die("Could not create socket: [$errorcode] $errormsg\n");
+}
+
+echo "Socket created\n";
+
+if (!socket_bind($socket, "0.0.0.0", $port)) {
+	$errorcode = socket_last_error();
+	$errormsg = socket_strerror($errorcode);
+
+	die("Could not bind socket: [$errorcode] $errormsg\n");
+}
+
+echo "Socket bind OK\n";
+
+while (true) {
+	$r = socket_recvfrom($socket, $buf, 2048, 0, $name, $port);
+
+	$buf = substr($buf, 5);
+	$buf = preg_replace("/\x{F3}.\x{00}/", "\n", $buf);
+	$buf = explode("\n", $buf);
+
+	foreach ($buf as &$value) {
+		$value = preg_split("/\t/", $value);
+		$length = sizeof($value);
+
+		if ($length >= 8) {
+			send_results($value);
+		}
+	}
+}
+
+socket_close($socket);
+?>
